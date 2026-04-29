5b995dce746f7ae20e092394131517eed862f7ce
cirobuono1962-alt <ciro.buono1962@gmail.com>
2026-04-29 09:31:52 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'meddispenser-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
